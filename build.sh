9a953d63656f09402ac7cde1d39382cf9dc974a9
JesusMedinaGomez <jesus.emg01@gmail.com>
2025-09-05 14:02:30 -0600

    build sh 3

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
 set -o errexit
+
+# Instalar dependencias
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Migraciones y estáticos
 python manage.py collectstatic --no-input
 python manage.py migrate
